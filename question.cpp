315b8171a01b64aaa2c490cc00bf1250d3ba2094
Sakshi Sinha <128817418+SakshiSinha0901@users.noreply.github.com>
2024-07-03 23:33:20 +0530

    Update question.cpp

--- question.cpp
+++ question.cpp
@@ -42,3 +42,115 @@ int main(){
     }
 }
 
+//print all pairs
+// #include <iostream>
+// #include <algorithm>
+// using namespace std;
+// void printAllPairs(int arr[],int n){
+//     for (int i=0; i<n; i++){
+//         for(int j= 0 ; j<n; j++){
+//             cout<<arr[i] << "," << arr[j]<< endl;
+//         }
+//     }
+// }
+
+// int main(){
+//     int arr[]= {10,20,30,40};
+//     int n= 4;
+//     printAllPairs(arr, n);
+// }
+
+// printing half factor
+// #include <iostream>
+// #include <algorithm>
+// using namespace std;
+// void printAllPairs(int arr[],int n){
+//     for (int i=0; i<n; i++){
+//         for(int j= i ; j<n; j++){
+//             cout<<arr[i] << "," << arr[j]<< endl;
+//         }
+//     }
+// }
+
+// int main(){
+//     int arr[]= {10,20,30,40};
+//     int n= 4;
+//     printAllPairs(arr, n);
+// }
+
+//knowing different output
+// #include <iostream>
+// #include <algorithm>
+// using namespace std;
+// void printAllPairs(int arr[],int n){
+//     for (int i=0; i<n; i++){
+//         for(int j= n-1 ; j>=i; j++){
+//             cout<<arr[i] << "," << arr[j]<< endl;
+//         }
+//     }
+// }
+
+// int main(){
+//     int arr[]= {10,20,30,40};
+//     int n= 4;
+//     printAllPairs(arr, n);
+// }
+//Two sum
+// #include <iostream>
+// #include <algorithm>
+// using namespace std;
+
+// bool checkTwoSum (int arr[], int n ,int target){
+//     //check all pairs
+//     for(int i=0; i<n ; i++){
+//         for (int j=0; j<n ; j++){
+//             if(arr[i] + arr[j] == target){
+//                 return true;
+//             }
+//         }
+//     }
+//     return false;
+// }
+// int main(){
+//     int arr[]= {10,20,30,40};
+//     int n= 4;
+//     bool ans = checkTwoSum(arr,n,60);
+//     if(ans == true){
+//         cout<<"Pair found."<<endl;
+//     }
+//     else{
+//         cout<<"Pair not found."<<endl;
+//     }
+//     return 0;
+// }
+
+// by pair method
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+pair<int,int> checkTwoSum (int arr[], int n ,int target){
+    //assuming (-1,-1) as no answer found
+    pair<int,int> ans = make_pair(-1,-1);
+    for(int i=0; i<n ; i++){
+        for (int j=0; j<n ; j++){
+            if(arr[i] + arr[j] == target){
+                ans.first = arr[i];
+                ans.second = arr[j];
+            }
+        }
+    }
+    return ans;
+}
+int main(){
+    int arr[]= {10,20,30,40};
+    int n= 4;
+    pair<int,int> ans = checkTwoSum(arr,n,60);
+    if(ans.first == -1 && ans.second == -1 ){
+        cout<<"Pair notfound."<<endl;
+    }
+    else{
+        cout<<"Pair found."<<endl;
+    }
+    return 0;
+}
